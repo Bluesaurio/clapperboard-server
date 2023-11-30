d355872779e8bc5aa9d583df998cd4099ac60dd4
Bluesaurio <adriansiquiervalles@gmail.com>
2023-11-30 13:23:40 +0100

    auth routes done

--- models/User.model.js
+++ models/User.model.js
@@ -1,8 +1,14 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the User model to whatever makes sense in this case
+
 const userSchema = new Schema(
   {
+    username: {
+      type:String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
     email: {
       type: String,
       required: [true, 'Email is required.'],
@@ -13,10 +19,26 @@ const userSchema = new Schema(
     password: {
       type: String,
       required: [true, 'Password is required.']
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user"
+    },
+    profilePic: {
+      type: String,
+      default:"https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg"
+    },
+    firstName: String,
+    lastName: String,
+    bio: String,
+    location: String,
+    pronouns: {
+      type: String,
+      enum: ["he/him", "she/her", "they/them"]
     }
   },
-  {
-    // this second object adds extra properties: `createdAt` and `updatedAt`    
+  { 
     timestamps: true
   }
 );
