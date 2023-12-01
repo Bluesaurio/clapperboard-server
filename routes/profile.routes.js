02f86ce8eba5d5003821bd57cd9b948624a2c634
raulgp <raul.garrigos@hotmail.com>
2023-12-01 15:30:11 +0100

    Changes in profile routes about edit

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -3,16 +3,32 @@ const router = require("express").Router();
 
 // GET "/api/profile/:userId" para conseguir data sobre el usuario
 
-router.get("/:userId", async (req,res,next) => {
-    try {
-        const response = await User.findById(req.params.userId)
-        console.log(response);
-        res.json(response)
-    } catch (error) {
-        next(error)
-    }
-})
+router.get("/:userId", async (req, res, next) => {
+  try {
+    const response = await User.findById(req.params.userId);
+    console.log(response);
+    res.json(response);
+  } catch (error) {
+    next(error);
+  }
+});
 
 // PUT "/api/profile" para editar data del usuario en la edición del perfil
+router.put("/", async (req, res, next) => {
+  const { firstName, lastName, location, bio, pronouns } = req.body;
 
-module.exports = router
+  try {
+    await User.findOneAndUpdate({
+      firstName,
+      lastName,
+      location,
+      bio,
+      pronouns,
+    });
+    res.json("Profile updated");
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
--- routes/upload.routes.js
+++ routes/upload.routes.js
@@ -3,14 +3,12 @@ const uploader = require("../middlewares/cloudinary.config.js");
 
 // POST "/api/upload" para acceder a cloudinary y actualizar la imagen de perfil
 
-router.post("/", uploader.single("image"), (req,res,next) =>{
-    if (!req.file) {
-        next("No file uploaded!");
-        return;
-    }
-    res.json({imageUrl: req.file.path})
+router.post("/", uploader.single("image"), (req, res, next) => {
+  if (!req.file) {
+    res.status(400).json({ errorMessage: "Ha habido un error con la imagen" });
+    return;
+  }
+  res.json({ imageUrl: req.file.path });
 });
 
-
-
-module.exports= router
+module.exports = router;
