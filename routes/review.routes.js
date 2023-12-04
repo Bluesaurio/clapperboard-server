89692f5715847a8698ca2307fbd25e260845ad08
raulgp <raul.garrigos@hotmail.com>
2023-12-04 18:14:42 +0100

    Route to get all reviews from a movie

--- routes/review.routes.js
+++ routes/review.routes.js
@@ -4,30 +4,37 @@ const isTokenValid = require("../middlewares/auth.middlewares");
 const router = require("express").Router();
 
 // POST "/api/review/:movieId" para crear una review
+router.post("/:movieId", isTokenValid, async (req, res, next) => {
+  console.log("RATING AQUI", req.body);
+  // console.log("AQUI ESTA EL REQ.PAYLOAD", req.payload)
 
-router.post("/:movieId", isTokenValid, async (req,res,next) => {
-   console.log("RATING AQUI", req.body)
-    // console.log("AQUI ESTA EL REQ.PAYLOAD", req.payload)
+  // Primero buscar reseñas de esa pelicula y de ese User, y si se encuentra una, se envía un error de "no puedes volver a crear una reseña"
+  try {
+    const response = await Review.create({
+      rating: req.body.rating,
+      text: req.body.text,
+      filmId: req.params.movieId,
+      creatorId: req.payload._id,
+      creator: req.payload.username,
+    });
+    res.json(response);
+  } catch (error) {
+    next(error);
+  }
+});
 
-    // Primero buscar reseñas de esa pelicula y de ese User, y si se encuentra una, se envía un error de "no puedes volver a crear una reseña"
-    try {
-        const response = await Review.create({
-            rating: req.body.rating,
-            text: req.body.text,
-            filmId: req.params.movieId,
-            creatorId: req.payload._id,
-            creator:req.payload.username
-            
-        })
-        res.json(response)
-    } catch (error) {
-        next(error)
-    }
-})
+// GET "/api/review/:movieId" para conseguir todas las reviews de una película
+router.get("/:movieId", async (req, res, next) => {
+  try {
+    const response = await Review.find({ filmId: req.params.movieId });
+    console.log(response);
+    res.json(response);
+  } catch (error) {
+    next(error);
+  }
+});
 
 // PUT "/api/review/:reviewId" para editar una review específica
-
-
 
 // DELETE "/api/review/:reviewId" para eliminar una review
 
